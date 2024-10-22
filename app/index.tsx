9d0db9f3b49a9873a55de73acf3079dcd64804c3
ratanakhek22 <ratanakhek22@gmail.com>
2024-10-22 13:01:20 -0500

    back end prep

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,11 @@
-import { Text, View } from "react-native";
+import { Text, View, Button } from "react-native";
+import { useRouter } from "expo-router";
+import { useEffect, useState, Suspense } from "react";
+import { SQLiteProvider, useSQLiteContext, type SQLiteDatabase } from "expo-sqlite";
 
 export default function Index() {
+  const router = useRouter()
+
   return (
     <View
       style={{
@@ -9,7 +14,60 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Suspense fallback={<Text>Loading. . .</Text>}>
+        <SQLiteProvider databaseName="myDatabase.db" onInit={migrateDbIfNeeded} useSuspense>
+          <Text>Home Page</Text>
+          <Info/>
+        </SQLiteProvider>
+      </Suspense>
     </View>
   );
+}
+
+interface User {
+  username: string;
+  password: string;
+}
+
+export function Info() {
+  const db = useSQLiteContext();
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    async function setup() {
+      const result = await db.getAllAsync<User>('SELECT * FROM users');
+      setUsers(result);
+    }
+    setup();
+  }, []);
+
+  return (
+    <View >
+      {users.map((user, index) => (
+        <View key={index}>
+          <Text>{`${user.username} - ${user.password}`}</Text>
+        </View>
+      ))}
+    </View>
+  );
+}
+
+async function migrateDbIfNeeded(db: SQLiteDatabase) {
+  const DATABASE_VERSION = 1;
+  let obj = await db.getFirstAsync<{ user_version: number }>(
+    'PRAGMA user_version'
+  );
+  let currentDbVersion = obj!.user_version
+
+  console.log(currentDbVersion)
+  await db.execAsync(`
+    PRAGMA journal_mode = 'wal';
+    CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY NOT NULL, username TEXT NOT NULL, password TEXT NOT NULL);
+  `);
+  await db.runAsync('INSERT INTO users (username, password) VALUES (?, ?)', 'tom1', 'password123');
+  await db.runAsync('INSERT INTO users (username, password) VALUES (?, ?)', 'alice3', '12345');
+
+  console.log(currentDbVersion)
+
+  await db.execAsync(`PRAGMA user_version = 1`);
 }
