1c7827d04880becb4ce0b1f28b759f6acf32f27f
ratanakhek22 <ratanakhek22@gmail.com>
2024-10-22 19:51:24 -0500

    login page and nav

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,7 @@
-import { Text, View, Button, TextInput } from "react-native";
+import { Text, View, TextInput, StyleSheet, Image, Button } from "react-native";
 import { useRouter } from "expo-router";
 import { useEffect, useState, Suspense } from "react";
 import { SQLiteProvider, useSQLiteContext, type SQLiteDatabase } from "expo-sqlite";
-import { NavigationAction, NavigationContainer } from "@react-navigation/native";
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator();
@@ -20,21 +19,81 @@ export default function Index() {
     >
       <Suspense fallback={<Text>Loading. . .</Text>}>
         <SQLiteProvider databaseName="myDatabase.db" onInit={migrateDbIfNeeded} useSuspense>
-          <Text>Home Page</Text>
-          <TextInput
-            placeholder = "login"
-            keyboardType = "email-address"
-            />
-            <TextInput
-            placeholder = "password"
-            keyboardType = "email-address"
-            />
-          {/*<Info/>*/}
+          <View
+            style={styles.parentContainer}
+          >
+            <View
+              style={styles.pad}
+            >
+              <Image
+                style={styles.logo}
+                source={require('../assets/images/app_logo.png')}
+                resizeMode="contain"
+              />
+            </View>
+            <View
+              style={styles.loginContainer}
+            >
+              <Text
+                style={styles.title}
+                >
+                Login</Text>
+              <TextInput
+                placeholder = "login"
+                keyboardType = "email-address"
+                style={styles.input}
+                />
+                <TextInput
+                placeholder = "password"
+                keyboardType = "email-address"
+                style={styles.input}
+                />
+                <Button
+                  onPress={() => router.navigate('../register/page')}
+                  title='Register Here'
+                />
+            </View>
+          </View>
         </SQLiteProvider>
       </Suspense>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  parentContainer: {
+    padding: 0,
+    flex: 1,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    width: 240,
+  },
+  title: {
+    fontSize: 50,
+    alignSelf: 'center',
+  },
+  loginContainer: {
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 100,
+    flex: 3,
+  },
+  pad: {
+    borderColor: 'black',
+    borderWidth: 1,
+    flex: 2,
+  },
+  logo: {
+    borderColor: 'black',
+    borderWidth: 1,
+    alignSelf: 'center',
+    flex: 1,
+  },
+});
 
 interface User {
   username: string;
@@ -65,21 +124,30 @@ export function Info() {
 }
 
 async function migrateDbIfNeeded(db: SQLiteDatabase) {
-  const DATABASE_VERSION = 1;
+  // FOR DEBUGGING: Reset users TABLE
+  // await db.runAsync('DROP TABLE IF EXISTS users');
+  // await db.execAsync(`PRAGMA user_version = 0`);
+
+  // Get user Db version
   let obj = await db.getFirstAsync<{ user_version: number }>(
     'PRAGMA user_version'
   );
   let currentDbVersion = obj!.user_version
 
-  console.log(currentDbVersion)
-  await db.execAsync(`
-    PRAGMA journal_mode = 'wal';
-    CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY NOT NULL, username TEXT NOT NULL, password TEXT NOT NULL);
-  `);
-  await db.runAsync('INSERT INTO users (username, password) VALUES (?, ?)', 'tom1', 'password123');
-  await db.runAsync('INSERT INTO users (username, password) VALUES (?, ?)', 'alice3', '12345');
+  // FOR DEBUGGING: print user version
+  // console.log(currentDbVersion)
 
-  console.log(currentDbVersion)
+  // Migrate Db version 1
+  if (currentDbVersion < 1) {
+    await db.execAsync(`
+      PRAGMA journal_mode = 'wal';
+      CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY NOT NULL, username TEXT NOT NULL, password TEXT NOT NULL);
+    `);
+    await db.runAsync('INSERT INTO users (username, password) VALUES (?, ?)', 'tom1', 'password123');
+    await db.runAsync('INSERT INTO users (username, password) VALUES (?, ?)', 'alice3', '12345');
 
-  await db.execAsync(`PRAGMA user_version = 1`);
+    await db.execAsync(`PRAGMA user_version = 1`);
+    currentDbVersion = 1
+  }
+  // console.log(currentDbVersion)
 }
